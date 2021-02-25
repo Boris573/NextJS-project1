7890111fc88991324627b42d00ad130b98164f91
Boris573 <chepicov.boris@mailr.u>
2021-02-25 17:53:09 +0300

    last commit

--- src/components/textFromServer.tsx
+++ src/components/textFromServer.tsx
@@ -1,21 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from '../../styles/Home.module.css'
-
-
-const getTextFromServer = async() => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/59`)
-    const textFromServer = await response.json()
-    return textFromServer.body
-}
 
 const TextFromServer = () => {
 
     const [text, setText] = useState<string>('');
-   
-    const textFromServer = getTextFromServer()
-    textFromServer.then(text => setText(text))
 
-      return(
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts/59`)
+            const obj = await response.json();
+            setText(obj.body);
+        })()
+    }, []);
+
+    return(
         <div className={styles.post}>
             Text from server: <p>{text}</p>
         </div>
